ec1331f6b24b9ccb420b16e10229dd594767c660
Gabriel Borja <gabyborja1994@gmail.com>
2021-01-15 10:47:43 -0500

    Add exercise 5 file on more variables and printing

---
+++ ex5.py
@@ -0,0 +1,25 @@
+name = 'Zed A. Shaw'
+age = 35 
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'white'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy.")
+print("Actually that's not too heavy.") 
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+# 1 inch = 2.54 cm
+height_cm = height * 2.54
+print(f"{height} inches is equal to {height_cm} centimeters.")
+
+# 1 pound = 0.45 kg
+weight_kg = weight * 0.45
+print(f"{weight} pounds is equal to {weight_kg} kilograms.")
