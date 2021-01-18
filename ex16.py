6750a63c2d636169f1d789c0f7792d8db949dc4d
Gaby Borja <gabyborja1994@gmail.com>
2021-01-18 17:06:56 -0500

    Make changes to write function to reduce repetition

--- ex16.py
+++ ex16.py
@@ -22,12 +22,9 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(f"""{line1}
+{line2}
+{line3}""")
 
 print("And finally,  we close it.")
 target.close()
