272b82a3dc0060fb1da5f8c2c07da37813337298
Gaby Borja <gabyborja1994@gmail.com>
2021-01-18 12:35:41 -0500

    Add ex12.py file on Prompting People

---
+++ ex12.py
@@ -0,0 +1,9 @@
+#Prompting people
+
+age = input("How old are you? ")
+height = input("how tall are you? ")
+weight = input("How much do you weight? ")
+
+print(f"So, you're {age} old, {height}, tall and {weight} heavy.")
+
+print("How old are you?", input())
