27ea493a0ef3bb5e77104fa0cc1f419ad889031f
Gaby Borja <gabyborja1994@gmail.com>
2021-01-21 09:25:08 -0500

    Add debug print statements for reversing ex23 script

--- ex23_btc.py
+++ ex23_btc.py
@@ -14,8 +14,20 @@ def print_line(line, encoding, errors):
     next_lang = line.strip() """
 
 
-languages = open("languages.txt", 'rb')
+languages = open("languages.txt", 'rb') # open languages as bytes
 line = languages.readline()
 print(f">>> {line}")
-strip_line = line.strip()
-print(f">> {strip_line}")
+strip_line = line.strip() #strips the trailing \n
+print(f">> {strip_line}") 
+
+line2 = languages.readline()
+print(f'>>> {line2}')
+
+""" whole = languages.read()
+print(whole) """
+
+raw_bytes = line2
+print(f"raw bytes is \n {raw_bytes}")
+# Decode bytes
+cooked_string = raw_bytes.decode('utf-8', errors='strict')
+print(f"cooked string is \n {cooked_string}")
