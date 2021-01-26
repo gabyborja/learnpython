59a635b3e14ce9c5e59473d872d7dbdf998e35de
Gaby Borja <gabyborja1994@gmail.com>
2021-01-26 06:41:07 -0500

    Add ex28 file on Boolean Practice

---
+++ ex28.py
@@ -0,0 +1,20 @@
+1. True
+2. False
+3. True and False -> False
+4. True
+5. True or True -> True
+6. True
+7. False
+8. True
+9. False
+10. False
+11. True
+12. False
+13. True
+14. False
+15. False
+16. False
+17. True
+18. True and True -> True
+19. False 
+20. False
