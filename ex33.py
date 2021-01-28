7e6402ed8794b2dbf792bf72a69c3296566d60de
Gaby Borja <gabyborja1994@gmail.com>
2021-01-28 18:01:49 -0500

    Add ex33 Study Drills

--- ex33.py
+++ ex33.py
@@ -1,22 +1,29 @@
 i = 0 
 numbers = []
 
-def call_loop(last_num): 
+def call_loop(last_num, increment): 
     i = 0
     while i < last_num:
         print(f"At the top i is {i} ")
         numbers.append(i)
 
-        i = i + 1
+        i = i + increment
         print("Numbers now: ", numbers)
         print(f"At the bottom i is {i}")\
 
 print("Loop until what number?")
 input_num = int(input("> "))
+print("What is the step size?")
+input_step = int(input("> "))
 
-call_loop(input_num)
+call_loop(input_num, input_step)
 
 print("The numbers: ")
 
 for num in numbers:
     print(num)
+
+print("Printing with range")
+x = range(0, input_num, input_step)
+for n in x: 
+    print(n)
