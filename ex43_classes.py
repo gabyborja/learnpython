bd4d3310c0c67b958e7070730a655df4673ab99f
Gaby Borja <gabyborja1994@gmail.com>
2021-02-01 20:39:56 -0500

    Add ex43 file on Basic Object-Oriented Analysis and Design

---
+++ ex43_classes.py
@@ -0,0 +1,82 @@
+class Scene(object):
+
+    def enter(self):
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter().")
+        exit(1)
+        pass
+
+
+class Engine(object):
+    
+    def __init__(self, scene_map):
+        self.scene_map = scene_map
+        pass
+
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+            # be sure to print out the last scene
+            current_scene.enter()
+        pass
+
+class Death(Scene):
+
+    def enter(self):
+        pass
+
+class CentralCorridor(Scene):
+
+    def enter(self):
+        pass
+
+class LaserWeaponArmory(Scene):
+
+    def enter(self):
+        pass
+
+class TheBridge(Scene):
+
+    def enter(self):
+        pass
+
+class EscapePod(Scene):
+
+    def enter(self):
+        pass
+
+class Finished(Scene):
+    
+    def enter(self):
+        print("You're finished. Well done.")
+
+class Map(object):
+
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death(),
+        'finished': Finished()
+    }
+
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self, scene_name):
+        val = Map.scenes.get(scene_name)
+        return val
+
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
+
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
