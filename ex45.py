b506771eb309798cbb136834580d4a81a67531ab
Gaby Borja <gabyborja1994@gmail.com>
2021-02-05 06:29:42 -0500

    Add ex45 file on Make Your Own Game

---
+++ ex45.py
@@ -0,0 +1,24 @@
+class Map(object):
+    def __init__(self):
+        pass
+
+class Engine(object):
+    def __init__(self):
+        pass
+
+class Room(object):
+    def __init__(self):
+        pass
+
+class CopperRoom(Room):
+    def __init__(self):
+        pass
+
+class JadeRoom(Room):
+    def __init__(self):
+        pass
+
+class CrystalRoom(Room):
+    def __init__(self):
+        pass
+    
