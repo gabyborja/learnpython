57b98b7f255de0720a0fcba13132a1b30e7a4559
Gaby Borja <gabyborja1994@gmail.com>
2021-02-07 16:48:14 -0500

    Add Engine while loop and loop through room list

--- ex45.py
+++ ex45.py
@@ -6,18 +6,6 @@
 4. Your runner will need to know about these rooms, so make a class that runs them and knows about them. There are plenty of ways to do this, but consider having each room return what room is next or setting a variable of what room is next. 
 """
 
-class Map(object):
-    
-    def __init__(self, room):
-        self.room_list = {'copper_room':CopperRoom(), 'jade_room':JadeRoom(), 'crystal_room':CrystalRoom(), 'finished':Finished(), 'death':Death()}
-        print("Map __init__")
-        self.room = room
-        print(self.room)
-    
-    def opening_room(self):
-        print(f"{self.room_list.get((self.room))}")
-        return self.room_list.get((self.room))
-
 class Engine(object):
     def __init__(self, map):
         self.map = map
@@ -28,7 +16,16 @@ class Engine(object):
         print(f"self.map {self.map}")
         room = self.map.opening_room()
         print(f"Engine play opening_room {room}")
-        next_room = room.enter() 
+        last_room = self.map.next_room('finished')
+        print(f"Engine play last_room {last_room}")
+
+        while room != last_room:
+            print(f"Engine play enter {room}")
+            next_room_str = room.enter()
+            print(f"Engine next_room {next_room_str}")
+            room = self.map.next_room(next_room_str)
+
+        room.enter()
 
 class Room(object):
     def __init__(self):
@@ -73,6 +70,28 @@ class Finished(Room):
     def enter(self):
         print("Finished enter")
         exit(1)
+
+class Map(object):
+    room_list = {
+        'copper_room':CopperRoom(), 
+        'jade_room':JadeRoom(), 
+        'crystal_room':CrystalRoom(), 
+        'finished':Finished(), 
+        'death':Death()
+    }
+
+    def __init__(self, start_room):
+        print("Map __init__")
+        self.start_room = start_room
+        print(f"Map __init__ {self.start_room}")
+    
+    def next_room(self, next_room):
+        next_room_object = self.room_list.get(next_room) 
+        return next_room_object
+
+    def opening_room(self):
+        print(f"Opening_room {self.next_room(self.start_room)}")
+        return self.next_room(self.start_room)
 
 a_map = Map('copper_room')
 a_game = Engine(a_map)
