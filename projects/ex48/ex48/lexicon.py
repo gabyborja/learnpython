6f9049f6448dfd334b1c7d87463a5f3b1f33b235
Gaby Borja <gabyborja1994@gmail.com>
2021-02-11 07:02:00 -0500

    Add scan function to lexicon

--- projects/ex48/ex48/lexicon.py
+++ projects/ex48/ex48/lexicon.py
@@ -1,12 +1,25 @@
-class Room(object):
+def scan(user_input):
+    """Scan user input for words that fit in lexicon
+    1. Break up sentence
+    2. Scan words for their type
+    3. Return tuple
+    4. Make sentence out of them """
+    print("lexicon Scan function")
+    print(f"{user_input}")
 
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-        self.paths = {}
+    words = user_input.split()
+    print(f"words {words}")
+    print(f"length of list {len(words)}")
+
+    for i in words:
+        if words(i) in ['north', 'south', 'east']: #check if word belongs to lexicon
+            return # return tuple 
     
-    def go(self, direction):
-        return self.paths.get(direction, None)
+    # join tuples to form sentence
+    if words == ['north']:
+        return [('direction', 'north')]
     
-    def add_paths(self, paths):
-        self.paths.update(paths)
+    
+    
+
+    
