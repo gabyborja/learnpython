e67338e3c5d1978015096fd2b3862f1a3891bd16
Gaby Borja <gabyborja1994@gmail.com>
2021-02-11 06:40:04 -0500

    Fix typos in unit test

--- projects/ex48/tests/lexicon_tests.py
+++ projects/ex48/tests/lexicon_tests.py
@@ -4,7 +4,7 @@ from ex48 import lexicon
 def test_directions():
     assert_equal(lexicon.scan("north"), [('direction', 'north')])
     result = lexicon.scan("north south east")
-    assert_equa(result, [('direction', 'north'),
+    assert_equal(result, [('direction', 'north'),
                          ('direction', 'south'),
                          ('direction', 'east')])
 
@@ -33,6 +33,5 @@ def test_errors():
                             [('error', "ASDFASDFASADAFASF")])
     result = lexicon.scan("bear IAS princess")
     assert_equal(result, [('noun', 'bear'),
-                          ('error', IAS),
+                          ('error', 'IAS'),
                           ('noun', 'princess')])
-                          
