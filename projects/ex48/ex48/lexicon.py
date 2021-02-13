289b4d990c9fc135a919c1b9d4b976107fff812c
Gaby Borja <gabyborja1994@gmail.com>
2021-02-13 08:56:40 -0500

    Add string case compatibility and to unit tests

--- projects/ex48/ex48/lexicon.py
+++ projects/ex48/ex48/lexicon.py
@@ -20,20 +20,21 @@ def scan(user_input):
     
 
     for word in words:
-        print(word)
-        if word in ['north', 'south', 'east']: # direction lexicon
+        word_check = word.lower() # Making sure to handle capitalization
+        print(word_check)
+        if word_check in ['north', 'south', 'east']: # direction lexicon
             sentence.append(('direction', word))
             print(f'in direction lexicon {word}')
             print(f'return {sentence}')
-        elif word in ['go', 'stop', 'kill', 'eat']: # verb lexicon
+        elif word_check in ['go', 'stop', 'kill', 'eat']: # verb lexicon
             sentence.append(('verb', word))
             print(f'in verb lexicon {word}')
             print(f'return {sentence}')
-        elif word in ['the', 'in', 'of', 'from', 'at', 'it']: # stop lexicon
+        elif word_check in ['the', 'in', 'of', 'from', 'at', 'it']: # stop lexicon
             sentence.append(('stop', word))
             print(f'in stop lexicon {word}')
             print(f'return {sentence}')
-        elif word in ['door', 'bear', 'princess', 'cabinet']: # noun lexicon
+        elif word_check in ['door', 'bear', 'princess', 'cabinet']: # noun lexicon
             sentence.append(('noun', word))
             print(f'in noun lexicon {word}')
             print(f'return {sentence}')
@@ -44,9 +45,5 @@ def scan(user_input):
                 sentence.append(('number', number))
             else:
                 sentence.append(('error', word))
-                
-    # TO-DO
-    # Checking for numbers, converting numbers, ValueError exception
-    # Lower case upper case -> checks
 
     return sentence
--- projects/ex48/tests/lexicon_tests.py
+++ projects/ex48/tests/lexicon_tests.py
@@ -3,32 +3,32 @@ from ex48 import lexicon
 
 def test_directions():
     assert_equal(lexicon.scan("north"), [('direction', 'north')])
-    result = lexicon.scan("north south east")
-    assert_equal(result, [('direction', 'north'),
+    result = lexicon.scan("NORTH south east")
+    assert_equal(result, [('direction', 'NORTH'),
                          ('direction', 'south'),
                          ('direction', 'east')])
 
 def test_verbs():
     assert_equal(lexicon.scan("go"), [('verb', 'go')])
-    result = lexicon.scan("go kill eat")
+    result = lexicon.scan("go kill EAT")
     assert_equal(result, [('verb', 'go'),
                           ('verb', 'kill'),
-                          ('verb', 'eat')])
+                          ('verb', 'EAT')])
 
 
 def test_stops():
     assert_equal(lexicon.scan("the"), [('stop', 'the')])
-    result = lexicon.scan("the in of")
+    result = lexicon.scan("the IN of")
     assert_equal(result, [('stop', 'the'),
-                          ('stop', 'in'),
+                          ('stop', 'IN'),
                           ('stop', 'of')])
 
 
 def test_nouns():
     assert_equal(lexicon.scan('bear'), [('noun', 'bear')])
-    result = lexicon.scan("bear princess")
+    result = lexicon.scan("bear PRINCESS")
     assert_equal(result, [('noun', 'bear'),
-                          ('noun', 'princess')])
+                          ('noun', 'PRINCESS')])
 
 
 def test_errors():
